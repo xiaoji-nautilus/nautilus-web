eb202dd96cfd2a3aced8a5c512d5fca6f87a202e
leon_xi <leon_xi@163.com>
2020-02-16 14:55:01 +0800

    更新发布判断

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -v
 
 echo "Uploading nautilus web files"
-ls -la $TRAVIS_BUILD_DIR/www
-sshpass -e scp -r stricthostkeychecking=no $TRAVIS_BUILD_DIR/www/* root@www.guobaa.com:/var/www/html/ncs/
+if [ -d "$TRAVIS_BUILD_DIR/www" ] then
+  ls -la $TRAVIS_BUILD_DIR/www
+  sshpass -e scp -r stricthostkeychecking=no $TRAVIS_BUILD_DIR/www/* root@www.guobaa.com:/var/www/html/ncs/
+else
+  echo "folder www is not exist."
+fi
