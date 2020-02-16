7e4a1a56b1341c2e22ef376b25c15066c45ea9d6
leon_xi <leon_xi@163.com>
2020-02-16 15:04:37 +0800

    Shell条件判断语法

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v
 
 echo "Uploading nautilus web files"
-if [ -d "$TRAVIS_BUILD_DIR/www" ] then
+if [ -d "$TRAVIS_BUILD_DIR/www" ]; then
   ls -la $TRAVIS_BUILD_DIR/www
   sshpass -e scp -r stricthostkeychecking=no $TRAVIS_BUILD_DIR/www/* root@www.guobaa.com:/var/www/html/ncs/
 else
