faddbf52f810dc4d573564e353779e6bfb7e6e67
leon_xi <leon_xi@163.com>
2020-02-16 15:19:32 +0800

    目录上传阿里云

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -4,6 +4,7 @@ echo "Uploading nautilus web files"
 if [ -d "$TRAVIS_BUILD_DIR/www" ]; then
   ls -la $TRAVIS_BUILD_DIR/www
   sshpass -e scp -r stricthostkeychecking=no $TRAVIS_BUILD_DIR/www/* root@www.guobaa.com:/var/www/html/ncs/
+  echo "folder www upload finished."
 else
   echo "folder www is not exist."
 fi
